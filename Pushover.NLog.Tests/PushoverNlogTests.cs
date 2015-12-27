0d1fa88123b79c5863b4b35765f5932ff1866034
Niels Maerten <niels@maerten.be>
2015-12-27 20:06:33 +0100

    Initial Commit

---
+++ Pushover.NLog.Tests/PushoverNlogTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pushover.NLog;
+using NLog;
+using NLog.Config;
+
+namespace Pushover.NLog.Tests
+{
+    [TestClass]
+    public class PushoverNLogTests
+    {
+        private readonly Logger logger;
+        /// <summary>
+        /// Configure PushoverTarget using NLog API
+        /// </summary>
+        public PushoverNLogTests()
+        {
+            //Get logging configuration
+            var logConfig = LogManager.Configuration ?? new LoggingConfiguration();
+
+            //Initialize PushoverTarget using API keys
+            var target = new PushoverTarget()
+            {
+                AppToken = "YOUR PUSHOVER APP TOKEN. ref: https://pushover.net ",
+                UserOrGroupKey = "YOUR PUSHOVER USER KEY. ref: https://pushover.net ",
+
+                //Optional properties
+                EmergencyMessageRetryInterval = 30,
+                EmergencyMessageExpiration = 3600,
+                ApiBase = "https://api.pushover.net/1/messages.json",
+                Device = null,
+                Layout = "${message}",
+                Title = "${level} event occurred",
+                Url = "http://example.com",
+                UrlTitle = "example.com"
+
+
+            };
+
+            //Register target with NLog
+            logConfig.AddTarget(target);
+            logConfig.LoggingRules.Add(new LoggingRule("*", LogLevel.Trace, target));
+
+            //Update configuration
+            LogManager.Configuration = logConfig;
+            LogManager.ReconfigExistingLoggers();
+        }
+
+        /// <summary>
+        /// Pump out a test message for every log level
+        /// </summary>
+        [TestMethod]
+        public void DebugTest()
+        {
+            logger.Debug("Debug test");
+            logger.Trace("Trace test");
+            logger.Info("Info test");
+            logger.Warn("Warn test");
+            logger.Error("Error test");
+            logger.Fatal("Fatal test");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# pushover-nlog
-Nlog target around Pushover-dotnet
+# Pushover.NLog
+An NLog target for Pushover. Built around pushover-dotnet, originally forked from https://github.com/mattmelling/pushover-dotnet
+
+## Quick start
+Configure using NLog API, or use nlog.config:  
+
+
+    //Get logging configuration
+    var logConfig = LogManager.Configuration ?? new LoggingConfiguration();
+
+    //Initialize PushoverTarget using API keys
+    var target = new PushoverTarget()
+    {
+        AppToken = "YOUR PUSHOVER APP TOKEN. ref: https://pushover.net ",
+        UserOrGroupKey = "YOUR PUSHOVER USER KEY. ref: https://pushover.net ",
+
+        //Optional properties
+        EmergencyMessageRetryInterval = 30,
+        EmergencyMessageExpiration = 3600,
+        ApiBase = "https://api.pushover.net/1/messages.json",
+        Device = null,
+        Layout = "${message}",
+        Title = "${level} event occurred",
+        Url = "http://example.com",
+        UrlTitle = "example.com"
+    };
+
+    //Register target with NLog
+    logConfig.AddTarget(target);
+    logConfig.LoggingRules.Add(new LoggingRule("*", LogLevel.Error, target));
+
+    //Update configuration
+    LogManager.Configuration = logConfig;
+    LogManager.ReconfigExistingLoggers();
+    
+Now, logs with ERROR or FATAL levels will be sent to your Pushover account:
+
+    var logger = LogManager.GetCurrentClassLogger();
+    logger.Error("Error test");
+    logger.Fatal("Fatal test");
